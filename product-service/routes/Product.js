2334580f6ed46876e02900c827c9fd2c4e86d116
chamika-damith <chamikadamith9@gmail.com>
2025-03-12 22:25:34 +0530

    create Get single product by ID route

--- product-service/routes/Product.js
+++ product-service/routes/Product.js
@@ -23,3 +23,16 @@ router.get('/all', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+
+// Get single product by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.findByPk(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
