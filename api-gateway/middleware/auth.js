3e86428609f4b94a50304ac856aa6821981bc867
chamika-damith <chamikadamith9@gmail.com>
2025-03-13 23:25:03 +0530

    complete config jwt auth

--- api-gateway/middleware/auth.js
+++ api-gateway/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next)=>{
-    const token = req.headers.authorization?.split(' ')[1]; // get the token from the request headers
+    const token = req.headers.authorization?.split(' ')[0]; // get the token from the request headers
 
     if(!token){
         return res.status(401).json({message: 'Access denied.No token provided'});
