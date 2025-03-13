49fc72a76f83425e2092cfe6b417b4c34daf8f8b
chamika-damith <chamikadamith9@gmail.com>
2025-03-13 22:48:10 +0530

    update

--- order-service/config/redis.js
+++ order-service/config/redis.js
@@ -1,13 +1,25 @@
-const redis = require('redis');
+const { createClient } = require('redis');
 require('dotenv').config();
 
-const redisClient = redis.createClient({
-    url:process.env.REDIS_URL
+const redisClient = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)
+    }
 });
 
-redisClient.on('connect',()=> console.log('Connect Redis client'));
-redisClient.on('error',()=> console.log('error Redis client'));
+redisClient.on('connect', () => console.log('Connected to Redis'));
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
-redisClient.connect();
+(async () => {
+    try {
+        await redisClient.connect();
+        console.log('Redis connection established');
+    } catch (error) {
+        console.error('Error connecting to Redis:', error);
+    }
+})();
 
 module.exports = redisClient;
